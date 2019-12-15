63fc89493fc8a8ff448030c735de62ca224900d1
Li Zhiyong <lizhiyong2000@gmail.com>
2019-12-15 15:57:37 +0800

    update

--- python/OpenCVDemo/main.py
+++ python/OpenCVDemo/main.py
@@ -4,15 +4,18 @@ from PyQt5.QtWidgets import QApplication
 
 from ClassificationWidget import ClassificationWidget
 from HistogramWidget import HistogramWidget
+from MatplotCanvasWidget import MatplotCanvasWidget
 from SegmentWidget import SegmentWidget
 
 app = QApplication(sys.argv)
 
 # widget = HistogramWidget()
-
+#
 # widget = SegmentWidget()
 
-widget = ClassificationWidget()
+# widget = ClassificationWidget()
+
+widget = MatplotCanvasWidget()
 
 widget.resize(1080, 640)
 widget.setWindowTitle('OpenCV Demo')
